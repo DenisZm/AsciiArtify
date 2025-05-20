49e7d27c477a239ad8f0fe2330f30a4890917ed2
Denis Zamosenchuk <DenisZm@users.noreply.github.com>
2025-05-20 21:07:40 +0000

    add userfull commands

---
+++ .misc/commands.sh
@@ -0,0 +1,25 @@
+# install homebrew 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo >> /home/codespace/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/codespace/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# brew tab completions
+brew install bash-completion
+
+brew install k9s
+
+# k3d completions
+sudo bash -c "k3d completion bash > /etc/bash_completion.d/k3d"
+
+# create k3d cluster
+k3d cluster create --agents 3 demo
+
+# deploy ArgoCD
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# get ArgoCD password
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+
